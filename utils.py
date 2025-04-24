69c83ce7fa16ee0e6085a198a803c5bfa3387596
Anura <150880291+Anura2425@users.noreply.github.com>
2025-04-23 17:16:26 -0700

    Add Introduction and start of data cleaning

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import requests
 
 def make_request(url):
+    """
+    Takes in an input url and returns the data as a json object
+    """
     response = requests.get(url)
     data = response.json()
-
-
-    
 
     return data
 
